0f02a32d30d43d897560569a315f343cafadeac7
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-06-26 13:08:17 +0530

    minor changes

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -3,8 +3,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Bookmark = () => {
   return (
-    <SafeAreaView className="px-4 my-6 bg-primary h-full">
-      <Text className="text-2xl text-white font-psemibold">Bookmark</Text>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="text-2xl px-4 my-6 text-white font-psemibold">
+        Bookmark
+      </Text>
     </SafeAreaView>
   );
 };
